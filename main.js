2e53d45a10908c5e026b79093aec1869ca8db656
martina di masi <martinadimasi24@gmail.com>
2024-08-01 18:18:00 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,79 +1,70 @@
-alert ("¡BIENVENIDO A TISIO JOYERIAS!")
 
-const nombre = prompt ("Para acceder a la tienda, ingresar tu nombre");
+alert("¡BIENVENIDO A TISIO JOYERÍAS!");
 
-function saludarPersona(){
-
-    alert("Bienvenido/a a Tisio Joyerías, " + nombre)
-};
-
-saludarPersona()
+const nombre = prompt("Para acceder a la tienda, ingresa tu nombre");
 
 let costoTotal = 0;
 
-let option;
+const productos = [
+    { 
+        id: 1, 
+        nombre: "Chain de Acero 800", 
+        precio: 20000 
+    },
+    { 
+        id: 2, 
+        nombre: "Aros Argolla Ancha", 
+        precio: 5700 
+    },
+    { 
+        id: 3, 
+        nombre: "Anillo Full Ice", 
+        precio: 80000 
+    },
+    { 
+        id: 4, 
+        nombre: "Aros Cristal colores", 
+        precio: 4300 
+    },
+    { 
+        id: 5, 
+        nombre: "Dije 0800 God", 
+        precio: 35000 
+    }
+];
 
-do {
-    option = parseInt(prompt(
-        "Seleccione el producto que desea llevar: \n\n1" +
-        "- Chain de Acero 800  $20.000 \n2" +
-        "- Aros Argolla Ancha  $5.700 \n3" +
-        "- Anillo Full Ice  $80.000 \n4" +
-        "- Aros Cristal colores  $4.300 \n5" +
-        "- Dije 0800 God  $35.000 \n0" +
-        "- Finalizar compra"
-    ));
-        
+let catalogo = "Catálogo:\n\n";
+productos.forEach(producto => {
+    catalogo += producto.id + " - " + producto.nombre + " | Precio: $ " + producto.precio + "\n";
+});
 
-    switch (option) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
+function comprarProducto() {
+    let option = parseInt(prompt(catalogo + "\n0 - Finalizar compra"));
 
-        let cantidad = parseInt (prompt("Ingresa la cantidad de productos que desea comprar"
+    if (option === 0) {
+        if (costoTotal > 0) {
+            alert("Gracias por tu compra, " + nombre + "! \nEl costo total es: $" + costoTotal);
+        } else {
+            alert("Gracias por visitar, ¡Vuelve pronto!");
+        }
+        return;
+    }
 
-        ));
+    if (option < 1 || option > productos.length) {
+        alert("Opción inválida");
+    } else {
+        const productoSeleccionado = productos.find(e => e.id === option);
+        const cantidad = parseInt(prompt("Ingresa la cantidad de productos que deseas comprar"));
 
-        let costoProducto = 0;
+        if (cantidad <= 0) {
+            alert("Cantidad inválida");
+        } else {
+            costoTotal += cantidad * productoSeleccionado.precio;
+            alert("Producto(s) añadido(s) al carrito.");
+        }
+    }
 
-        switch (option) {
-
-            case 1:
-                costoProducto = 20000;
-                break;
-            case 2:
-                costoProducto = 5700;
-                break;
-            case 3:
-                costoProducto = 80000;
-                break;
-            case 4:
-                costoProducto = 4300;
-                break;
-            case 5:
-                costoProducto = 35000;
-                break;
-            };
-        
-        costoTotal = cantidad * costoProducto;
-        alert("Producto(s) añadido(s) al carrito.");
-        break;
-        
-        case 0:
-            break;
-        
-        default: 
-            alert("Opción inválida");
-            break;
-    };
-
-
-} while (option !== 0);
-
-if (costoTotal > 0) {
-    alert ("Gracias por tu compra! \n" + "El costo total es: $" + costoTotal);
-} else {
-    alert ("Gracias por visitar,volve pronto!");
+    comprarProducto(); 
 }
+
+comprarProducto();
